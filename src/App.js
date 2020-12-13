bdacbffb437b1a0016a0f27199ca93c3b144f98a
SarahPruene <sarah.pruene91@gmail.com>
2020-12-13 20:07:08 +0100

    Added githubtepo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css'
 
 function App() {
-  return <div className='App'>Wetter App</div>
+  return (
+    <div className='App'>
+      <h1>Weather App</h1>
+      <footer>
+        This Project was coded by <span>Sarah</span> and is{' '}
+        <a href='https://github.com/SarahPruene/wetters-app-example' target='_blank'>open-sourced on Github</a>
+      </footer>
+    </div>
+  )
 }
 
 export default App
