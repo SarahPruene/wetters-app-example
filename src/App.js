c8948ae968c7cabc5a035f57f819ba9d02b0c060
SarahPruene <sarah.pruene91@gmail.com>
2020-12-13 20:25:46 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css'
 function App() {
   return (
     <div className='App'>
-      <h1>Weather App</h1>
-      <footer>
-        This Project was coded by <span>Sarah</span> and is{' '}
-        <a href='https://github.com/SarahPruene/wetters-app-example' target='_blank'>open-sourced on Github</a>
-      </footer>
+      <div className='container'>
+        <h1>Weather App</h1>
+        <footer>
+          This Project was coded by <span>Sarah</span> and is{' '}
+          <a
+            href='https://github.com/SarahPruene/wetters-app-example'
+            rel='noreferrer'
+            target='_blank'
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   )
 }
